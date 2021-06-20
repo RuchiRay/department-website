c4b68fd75f6d3631e6632f2d5ef1e12cf860dda9
Ruchi Ray <ruchiray18@gmail.com>
2021-06-20 19:39:17 +0530

    made dropdown clickable on phone

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let time = setInterval(myTimer, 1000);
 function myTimer() {
   let d = new Date();
   // let t = d.toUTCString()
-  d = d.toString()
-  let t = d.replace('GMT+0530 (India Standard Time)','')
+  d = d.toString();
+  let t = d.replace("GMT+0530 (India Standard Time)", "");
   document.getElementById("date").innerHTML = t;
 }
 
@@ -27,29 +27,59 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 
-  
   autoplay: {
     delay: 3000,
-  }
+  },
 });
 // for side menu
 const body = document.querySelector("body");
-    const navbar = document.querySelector(".nav-content");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    const topnav = document.querySelector('.topnav')
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      body.classList.add("disabled");
-      topnav.classList.remove("color");
+const navbar = document.querySelector(".nav-content");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const topnav = document.querySelector(".topnav");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+ 
+};
+cancelBtn.onclick = () => {
+  // body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+  // topnav.classList.add("color");
+};
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
+
+// for making dropdowns clickable
+let dropdowns = document.getElementsByClassName("dropdown");
+window.addEventListener("resize", () => {
+  for (let drop of dropdowns) {
+    if (window.innerWidth <= 1000) {
+      console.log("less");
+      drop.childNodes[3].classList.remove("hover");
+      console.log(drop.childNodes[3]);
     }
-    cancelBtn.onclick = ()=>{
-      body.classList.remove("disabled");
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-      topnav.classList.add("color");
+    else{
+      drop.childNodes[3].classList.add('hover')
     }
-    window.onscroll = ()=>{
-      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-    }
+  }
+});
+for (let drop of dropdowns) {
+  if (window.innerWidth <= 1000) {
+    console.log("less");
+    drop.childNodes[3].classList.remove("hover");
+    console.log(drop.childNodes[3]);
+  }
+}
+
+console.log(dropdowns);
+for (let drop of dropdowns) {
+  console.log(drop);
+  drop.addEventListener("click", (e) => {
+    drop.childNodes[3].classList.toggle("clicked");
+  });
+}
